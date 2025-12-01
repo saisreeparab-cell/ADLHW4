4e4e781ecc3cc4b9674ed4ef92c4e40eac0689f8
saisreeparab-cell <saisreeparab@gmail.com>
2025-11-30 23:32:18 -0600

    Update __init__.py

--- homework4_aug_4.2/homework/__init__.py
+++ homework4_aug_4.2/homework/__init__.py
@@ -3,5 +3,6 @@ from .clip import load as load_clip
 from .data import VQADataset, benchmark
 from .finetune import load as load_vlm
 from .finetune import train
+from . import data
 
 __all__ = ["BaseVLM", "VQADataset", "benchmark", "train", "load_vlm", "load_clip"]
